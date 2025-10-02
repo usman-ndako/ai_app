65a140848c704709c609d5cadce1f69113bc9aa1
Ndako Usman <103329764+usman-ndako@users.noreply.github.com>
2025-10-03 00:14:04 +0100

    Update summarizer_api.py

--- Day5/summarizer_api.py
+++ Day5/summarizer_api.py
@@ -59,7 +59,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://ai-app-chi-two.vercel.app/"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://ai-app-chi-two.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
